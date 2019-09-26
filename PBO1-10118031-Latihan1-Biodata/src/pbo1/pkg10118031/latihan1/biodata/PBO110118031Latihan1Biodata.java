08a74cc2b92f24e29bb8ee194e2a7352f25168b2
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 11:47:27 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan1-Biodata/src/pbo1/pkg10118031/latihan1/biodata/PBO110118031Latihan1Biodata.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118031.latihan1.biodata;
+
+/**
+ *
+ * @author Vendetta 
+ * NAMA : RIZKI ARIA PUTRA 
+ * KELAS : PBO1 
+ * NIM : 10118031
+ * Deskripsi Program : Program ini berisi program untuk menampilkan biodata
+ */
+public class PBO110118031Latihan1Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------------");
+        System.out.println("NAMA\t\t: Rizki Aria Putra");
+        System.out.println("ALAMAT\t\t: Jalan Sukagalih 124");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 13 November 2000 ");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Main Game");
+        System.out.println("MAKES\t\t: Ayam Goreng/Bakar");
+        System.out.println("MIKES\t\t: Cappuchino");
+    }
+}
